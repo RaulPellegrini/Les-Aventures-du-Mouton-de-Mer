bb8d1f0c5a456583c44b6dda2a0419c1f2a89513
Raul <raul_pellegrini@outlook.com>
2023-04-14 16:35:02 +0200

    Scene Transition

--- LAMM/Assets/Scripts/Scene Management/AreaExit.cs
+++ LAMM/Assets/Scripts/Scene Management/AreaExit.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class AreaExit : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
+    [SerializeField] private string sceneTransitionName;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
             SceneManager.LoadScene(sceneToLoad);
+            SceneManagement.Instance.SetTransitionName(sceneTransitionName);
         }
     }
 
