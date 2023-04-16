3dcdcaa5bf2b20b6d62647c1ebdc35730f5e3497
Raul <raul_pellegrini@outlook.com>
2023-04-16 10:29:43 +0200

    FadeTransition

--- LAMM/Assets/Scripts/Scene Management/AreaExit.cs
+++ LAMM/Assets/Scripts/Scene Management/AreaExit.cs
@@ -7,13 +7,32 @@ public class AreaExit : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
     [SerializeField] private string sceneTransitionName;
+
+    private float waitToLoadTime = 1f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
         {
-            SceneManager.LoadScene(sceneToLoad);
             SceneManagement.Instance.SetTransitionName(sceneTransitionName);
+            UIFade.Instance.FadeToBlack();
+            StartCoroutine(LoadSceneRoutine());
         }
     }
 
+
+    private IEnumerator LoadSceneRoutine()
+    {
+        //yield return new WaitForSeconds(waitToLoadTime);
+        
+        while (waitToLoadTime >= 0)
+        { 
+            waitToLoadTime -= Time.deltaTime;
+            yield return null;
+        
+        }
+        SceneManager.LoadScene(sceneToLoad);
+    }
 }
+
+
