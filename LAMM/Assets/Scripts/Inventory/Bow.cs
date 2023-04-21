70f3299afa5cb38b273d874113319a54eccb0d50
Raul <raul_pellegrini@outlook.com>
2023-04-21 14:57:03 +0200

    Bow Working!

--- LAMM/Assets/Scripts/Inventory/Bow.cs
+++ LAMM/Assets/Scripts/Inventory/Bow.cs
@@ -20,7 +20,7 @@ public class Bow : MonoBehaviour, IWeapon
     {
         myAnimator.SetTrigger(FIRE_HASH);
         GameObject newArrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, ActiveWeapon.Instance.transform.rotation);
-
+        newArrow.GetComponent<Projectile>().UpdateWeaponInfo(weaponInfo);
     }
 
     public WeaponInfo GetWeaponInfo() { return weaponInfo; }
