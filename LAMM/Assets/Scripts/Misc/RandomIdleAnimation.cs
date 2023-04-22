7c30565e731775ded87935ae3b5d5480bc6f4617
Raul <raul_pellegrini@outlook.com>
2023-04-22 19:32:05 +0200

    Ghost!

--- LAMM/Assets/Scripts/Misc/RandomIdleAnimation.cs
+++ LAMM/Assets/Scripts/Misc/RandomIdleAnimation.cs
@@ -11,7 +11,7 @@ public class RandomIdleAnimation : MonoBehaviour
     }
 
     private void Start()
-    {
+    {   if (!myAnimator) { return; }
         AnimatorStateInfo state = myAnimator.GetCurrentAnimatorStateInfo(0);
         
         myAnimator.Play(state.fullPathHash,-1, Random.Range(0.0f, 1.0f));
