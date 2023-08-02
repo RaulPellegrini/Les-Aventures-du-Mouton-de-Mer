585e190537c068bfe5e5f4755b2d0f2429977715
Raul <raul_pellegrini@outlook.com>
2023-08-02 21:26:07 +0200

    Tst

--- LAMM/Assets/Scripts/Enemies/Demon/DemonBoss.cs
+++ LAMM/Assets/Scripts/Enemies/Demon/DemonBoss.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -72,6 +73,7 @@ public class DemonBoss : MonoBehaviour, IEnemy
 
         else
         {
+            Console.WriteLine("summoning");
             state = State.Summoning;
         }
 
